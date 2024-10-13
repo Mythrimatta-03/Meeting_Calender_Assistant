bc75b6f046f88fb50c494342604226240c7dc7d8
Mythri Matta <mattamythri373@gmail.com>
2024-10-13 15:52:47 +0530

    Added

---
+++ src/main/java/org/jsp/Meeting_calender_Assistant/MeetingCalenderAssistantApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.Meeting_calender_Assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingCalenderAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingCalenderAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/Meeting_calender_Assistant/MeetingCalenderAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.Meeting_calender_Assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingCalenderAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
